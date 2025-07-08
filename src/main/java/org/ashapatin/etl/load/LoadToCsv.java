81695135b6b029a35ff443973b87d5f17cfae2f0
ShapatinAA <113165107+ShapatinAA@users.noreply.github.com>
2025-07-09 00:33:38 +0700

    Refactored load method from LoadToCsv class.

--- src/main/java/org/ashapatin/etl/load/LoadToCsv.java
+++ src/main/java/org/ashapatin/etl/load/LoadToCsv.java
@@ -1,6 +1,7 @@
 package org.ashapatin.etl.load;
 
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.dataformat.csv.CsvGenerator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.opencsv.CSVWriter;
@@ -20,25 +21,25 @@ import org.ashapatin.etl.model.WeatherData;
 
 public class LoadToCsv implements Load<WeatherAggregatedData> {
   private final String csvFilePath = "./weather_result.csv";
+  private final String datePattern = "yyyy-MM-dd'T'HH:mm'Z'";
 
   @Override
   public void load(WeatherAggregatedData data) throws Exception {
     try {
-      //TODO: refactor this
-      CsvMapper mapper = new CsvMapper();
-      TimeZone tz = TimeZone.getTimeZone("UTC");
-      DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
-      df.setTimeZone(tz);
-      mapper.setDateFormat(df);
-      CsvSchema schema = mapper.schemaFor(WeatherAggregatedData.class);
-      schema = schema.withColumnSeparator('\t');
+      DateFormat dateFormat = new SimpleDateFormat(datePattern);
 
-      ObjectWriter myObjectWriter = mapper.writer(schema);
-      File tempFile = new File("weather_result.csv");
-      FileOutputStream tempFileOutputStream = new FileOutputStream(tempFile);
-      BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(tempFileOutputStream, 1024);
-      OutputStreamWriter writerOutputStream = new OutputStreamWriter(bufferedOutputStream, "UTF-8");
-      myObjectWriter.writeValue(writerOutputStream, data);
+      CsvMapper csvMapper = new CsvMapper();
+      csvMapper.setDateFormat(dateFormat);
+      csvMapper.configure(CsvGenerator.Feature.STRICT_CHECK_FOR_QUOTING, true);
+
+      CsvSchema csvWeatherSchema = csvMapper
+          .typedSchemaFor(WeatherAggregatedData.class)
+          .withColumnSeparator('\t')
+          .withHeader();
+
+      ObjectWriter csvWriter = csvMapper.writer(csvWeatherSchema);
+      File csvFile = new File(csvFilePath);
+      csvWriter.writeValue(csvFile, data);
     }
     catch (IOException e) {
       throw new UnableToOpenCsvFile("Unable to open csv file on path: \"" + csvFilePath
